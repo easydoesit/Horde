49e7387e10da20fdbcc47190f9a56a593819de5f
Rhys Hughes <rhys.hughes@matrixlabs.org>
2024-11-07 14:19:51 -0800

    Added 'Increase Weapons Value' upgrade

--- src/scenes/playmode.ts
+++ src/scenes/playmode.ts
@@ -23,6 +23,7 @@ import { WheatState } from "../upgradesStandard/wheat";
 import { increaseOreValue } from "../utils/STANDARDUPGRADESCONSTANTS";
 import { IncreaseOreValueState } from "../upgradesStandard/increaseOreVal";
 import { IncreaseMiningSpeedState } from "../upgradesStandard/increaseMiningSpeed";
+import { IncreaseWeaponsValueState } from "../upgradesStandard/increaseWeaponVal";
 import { StructureFarms } from "../structures/structureFarms";
 import { KingdomState } from "../kingdoms/kingdomState";
 import { GUIStartScreen } from "../GUI/GUIStartScreen";
@@ -57,6 +58,7 @@ export class PlayMode extends Scene {
     public wheat:WheatState;
     public increaseOreValue:IncreaseOreValueState;
     public increaseMiningSpeed:IncreaseMiningSpeedState;
+    public increaseWeaponsValue:IncreaseWeaponsValueState;
 
     //Epic upgrades
     public allEpicUpgrades:any[];
