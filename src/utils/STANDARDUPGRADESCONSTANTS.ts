3e27684c1fe63a810ac44ea122ee73317f40dc20
Rhys Hughes <rhys.hughes@matrixlabs.org>
2024-11-07 14:19:51 -0800

    Added 'Increase Weapons Value' upgrade

--- src/utils/STANDARDUPGRADESCONSTANTS.ts
+++ src/utils/STANDARDUPGRADESCONSTANTS.ts
@@ -116,3 +116,43 @@ export const increaseMiningSpeed = {
     },
 
 }
+
+export const increaseWeaponsValue = {
+    name:'Increase Weapons Value',
+    structure:'Forge',
+    incrementValue:0.05,
+    upgradeMax:20,
+
+    effectValue: (upgradeLevel:number, upgradeMax:number, incrementalValue:number) => {
+    
+        if (upgradeLevel === 0) {
+            return 0;
+        }
+
+        const logTarget = 1;
+        const currentLevel = upgradeLevel;
+        const upgradeLimit = upgradeMax; 
+        const curveBalance = 5; 
+        const incrementValue = incrementalValue;
+
+        return logTarget * (Math.log(currentLevel)/Math.log(upgradeLimit)/curveBalance) + incrementValue;
+        
+    },
+
+    nextUpgradeCostGold: (upgradeLevel:number) => {
+        const nextUpgradeLevel = upgradeLevel + 1;
+        const baseGoldCost = 228;
+        const goldCostGrowthCurve = 3.4;
+    
+        let finalValue = baseGoldCost * goldCostGrowthCurve * nextUpgradeLevel;
+    
+        return finalValue;
+    },
+    nextUpgradeCostFarmers:(upgradeLevel:number) => {
+        return 0;
+    },
+    nextUpgradeCostResources:(upgradeLevel:number) => {
+        return 0;
+    },
+
+}
